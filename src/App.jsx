0d82a2b3b9bfcf9a0efb8915b4cde80adda2706e
RyanMagyar <ramagyar@umich.edu>
2025-01-07 18:06:45 -0500

    Initial setup for React Website with Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
