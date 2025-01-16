6cf6725ff469ac8728713f63f5cdbb7a02466949
RyanMagyar <ramagyar@umich.edu>
2025-01-15 23:52:13 -0500

    Added content to services cards and created icon assets for cards

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,4 @@
-import { benefits } from "../constants";
+import { services } from "../constants";
 import Section from "./Section";
 import Heading from "./Heading";
 import Arrow from "../assets/svg/Arrow";
@@ -15,7 +15,7 @@ const Services = () => {
         />
 
         <div className="flex flex-wrap gap-10 mb-10">
-          {benefits.map((item) => (
+          {services.map((item) => (
             <div
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
               style={{
