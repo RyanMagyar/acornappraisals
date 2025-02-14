aa45f10ae86a897c6ab912a17ed2ec657c0c7f0b
RyanMagyar <ramagyar@umich.edu>
2025-02-13 23:08:46 -0500

    removed form from layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,19 +6,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <title>Accorn Appraisal Services</title>
-      </head>
       <body>
         <div id="root">{children}</div>
-        <noscript>
-          <form name="contact" data-netlify="true" hidden>
-            <input type="text" name="Name" />
-            <input type="email" name="Email" />
-            <input type="tel" name="Phone" />
-            <textarea name="Message"></textarea>
-          </form>
-        </noscript>
       </body>
     </html>
   );
