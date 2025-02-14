319f43b93b36c80f9f319639f608bbe7bd681143
RyanMagyar <ramagyar@umich.edu>
2025-02-14 12:47:53 -0500

    changed headers on fetch

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -31,7 +31,7 @@ const ContactForm = ({ handleClose }) => {
       console.log(JSON.stringify(values));
       fetch("/_forms.html", {
         method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(values),
       });
 
